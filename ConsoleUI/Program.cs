d6432349ab78475f636fa2d539c8fd5bb53c698f
Abdul Kuzhiyil <Abdul.Kuzhiyil@ii-vi.com>
2023-03-06 12:14:01 +0000

    Initial Commit
    This is the starting point of the app

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DemoLibrary;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        List<IDataAccess> databases = new List<IDataAccess>()
+        {
+            new SqlDataAccess(),
+            new SqliteDataAccess()
+        };
+
+        foreach(var db in databases)
+        {
+            db.LoadConnectionString("demo");
+            db.LoadData("Select * from table");
+            db.SaveData("insert into table");
+        }
+        Console.ReadLine();
+    }
+}
---
+++ DemoLibrary/IDataAccess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public interface IDataAccess
+    {
+        string LoadConnectionString(string name);
+
+        void LoadData(string sql);
+
+        void SaveData(string sql);
+    }
+}
